ee01640f59a85b6876741627404a83b98c34089e
nicolas cros <none>
2021-03-11 20:11:01 +0100

    change theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+      ),
       home: RandomWords(),
     );
 

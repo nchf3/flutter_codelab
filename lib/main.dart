17a5d249cef9b4c79babc60bf33f185a534485b5
nicolas cros <none>
2021-03-11 19:27:46 +0100

    add favorite icon on the left + animation

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   @override
@@ -55,11 +56,25 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
 }
